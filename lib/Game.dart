e780c80bf5fe88ab025776ae845043daea7cb26f
Nrdias <natan.nrdias@hotmail.com>
2023-08-22 10:15:32 -0300

    finalizando o código

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget {
@@ -10,15 +12,30 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> {
 
   String _result = "";
-  final String _padrao = "images/padrao.png";
-  final String _pedra = "images/pedra.png";
-  final String _papel = "images/papel.png";
-  final String _tesoura = "images/tesoura.png";
+  final _list = ["images/pedra.png","images/papel.png","images/tesoura.png"];
+  var _machinechoice = AssetImage("images/padrao.png");
+
+  play(String user){
+    int aux = Random().nextInt(3);
+    String machine = _list[aux];
+    setState(() {
+      _machinechoice = AssetImage(machine);
+      if(machine == 'images/pedra.png' && user == 'images/tesoura.png') _result = 'Você perdeu';
+      else if(machine == 'images/pedra.png' && user == 'images/papel.png') _result = 'Você ganhou';
+      else if(machine == 'images/papel.png' && user == 'images/pedra.png') _result = 'Você perdeu';
+      else if(machine == 'images/papel.png' && user == 'images/tesoura.png') _result = 'Você ganhou';
+      else if(machine == 'images/tesoura.png' && user == 'images/papel.png') _result = 'Você perdeu';
+      else if(machine == 'images/tesoura.png' && user == 'images/pedra.png') _result = 'Você ganhou';
+      else _result = 'Empatou';
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("JoKenPo Game", textAlign: TextAlign.center)
       ),
       body: Column(
@@ -30,7 +47,7 @@ class _GameState extends State<Game> {
             style: TextStyle(fontSize: 20,
             fontWeight: FontWeight.bold))
           ),
-          Image.asset(_padrao),
+          Image(image: this._machinechoice),
           Text(_result, style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold
@@ -45,16 +62,16 @@ class _GameState extends State<Game> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
             GestureDetector(
-              onTap: (){},
-              child: Image.asset(_pedra, height: 95,)
+                onTap: () => play(_list[0]),
+                child: Image.asset(_list[0], height: 95)
             ),
             GestureDetector(
-                onTap: (){},
-                child: Image.asset(_papel, height: 95,)
+                onTap: () => play(_list[1]),
+                child: Image.asset(_list[1], height: 95)
             ),
             GestureDetector(
-                onTap: (){},
-                child: Image.asset(_tesoura, height: 95,)
+                onTap: () => play(_list[2]),
+                child: Image.asset(_list[2], height: 95)
             )
           ],
           )
